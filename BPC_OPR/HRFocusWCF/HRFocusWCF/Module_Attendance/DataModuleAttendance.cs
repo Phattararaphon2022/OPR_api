9b5654eeca262aa713dafb4aa894dc15a26e9c99
Phattaraphon61 <phattaraphon.pe.61@ubu.ac.th>
2023-03-28 15:02:35 +0700

    🔥 chore(DataModuleAttendance.cs): remove commented out code The commit removes commented out code in the DataModuleAttendance.cs file. The code is not needed and only clutters the file.
    🔥 refactor(DataModuleAttendance.cs): remove unused InputMTReason and InputMTLocation classes
    The InputMTReason and InputMTLocation classes were commented out and unused. This commit removes them from the codebase to improve code readability and maintainability.
    
    🚀 feat(DataModuleAttendance.cs): add InputTRTimeot class
    The InputTRTimeot class is added to the DataModuleAttendance.cs file. This class contains various properties that are used to store data related to time off requests. These properties include device_name, ip, username, company_code, worker_code, worker_name, timeot_id, timeot_doc, timeot_workdate, timeot_beforemin, timeot_normalmin, timeot_aftermin, timeot_note, location_code, reason_code, status, modified_by, modified_date, timeot_fromdate, timeot_todate, flag, and ot_data. This class is used to store data related to time off requests.

--- BPC_OPR/HRFocusWCF/HRFocusWCF/Module_Attendance/DataModuleAttendance.cs
+++ BPC_OPR/HRFocusWCF/HRFocusWCF/Module_Attendance/DataModuleAttendance.cs
@@ -9,41 +9,6 @@ namespace BPC_OPR
     public class DataModuleAttendance
     {
     }
-    #region InputMTYear
-    //[DataContract]
-    //public class InputMTYear
-    //{
-
-    //    [DataMember]
-    //    public string device_name { get; set; }
-    //    [DataMember]
-    //    public string ip { get; set; }
-    //    [DataMember]
-    //    public string username { get; set; }
-    //    [DataMember]
-    //    public string company_code { get; set; }
-    //    [DataMember]
-    //    public string year_id { get; set; }
-    //    [DataMember]
-    //    public string year_code { get; set; }
-    //    [DataMember]
-    //    public string year_name_th { get; set; }
-    //    [DataMember]
-    //    public string year_name_en { get; set; }
-    //    [DataMember]
-    //    public string year_fromdate { get; set; }
-    //    [DataMember]
-    //    public string year_todate { get; set; }
-    //    [DataMember]
-    //    public string year_group { get; set; }
-    //    [DataMember]
-    //    public string modified_by { get; set; }
-    //    [DataMember]
-    //    public DateTime modified_date { get; set; }
-    //    [DataMember]
-    //    public bool flag { get; set; }
-    //}
-    #endregion
 
     #region InputMTPeriod
     //[DataContract]
@@ -88,74 +53,6 @@ namespace BPC_OPR
 
     //}
     #endregion
-
-    #region InputMTReason
-    //[DataContract]
-    //public class InputMTReason
-    //{
-    //    [DataMember]
-    //    public string device_name { get; set; }
-    //    [DataMember]
-    //    public string ip { get; set; }
-    //    [DataMember]
-    //    public string username { get; set; }
-    //    [DataMember]
-    //    public string company_code { get; set; }
-    //    [DataMember]
-    //    public string reason_id { get; set; }
-    //    [DataMember]
-    //    public string reason_code { get; set; }
-    //    [DataMember]
-    //    public string reason_name_th { get; set; }
-    //    [DataMember]
-    //    public string reason_name_en { get; set; }
-    //    [DataMember]
-    //    public string reason_group { get; set; }
-
-    //    [DataMember]
-    //    public string modified_by { get; set; }
-    //    [DataMember]
-    //    public DateTime modified_date { get; set; }
-
-    //    [DataMember]
-    //    public bool flag { get; set; }
-    //}
-#endregion
-
-    #region InputMTLocation
-    //[DataContract]
-    //public class InputMTLocation
-    //{
-    //    [DataMember]
-    //    public string device_name { get; set; }
-    //    [DataMember]
-    //    public string ip { get; set; }
-    //    [DataMember]
-    //    public string username { get; set; }
-    //    [DataMember]
-    //    public string company_code { get; set; }
-    //    [DataMember]
-    //    public string location_id { get; set; }
-    //    [DataMember]
-    //    public string location_code { get; set; }
-    //    [DataMember]
-    //    public string location_name_th { get; set; }
-    //    [DataMember]
-    //    public string location_name_en { get; set; }
-    //    [DataMember]
-    //    public string location_detail { get; set; }
-    //    [DataMember]
-    //    public string location_lat { get; set; }
-    //    [DataMember]
-    //    public string location_long { get; set; }
-    //    [DataMember]
-    //    public string modified_by { get; set; }
-    //    [DataMember]
-    //    public DateTime modified_date { get; set; }
-    //    [DataMember]
-    //    public bool flag { get; set; }
-    //}
-#endregion
 
     #region InputMTPlanholiday
     [DataContract]
@@ -836,4 +733,55 @@ namespace BPC_OPR
         public string leave_data { get; set; }
     }
     #endregion  
+
+    #region InputTRTimeot
+    [DataContract]
+    public class InputTRTimeot
+    {
+        [DataMember]
+        public string device_name { get; set; }
+        [DataMember]
+        public string ip { get; set; }
+        [DataMember]
+        public string username { get; set; }
+        [DataMember]
+        public string company_code { get; set; }
+        [DataMember]
+        public string worker_code { get; set; }
+        [DataMember]
+        public string worker_name { get; set; }
+        [DataMember]
+        public string timeot_id { get; set; }
+        [DataMember]
+        public string timeot_doc { get; set; }
+        [DataMember]
+        public string timeot_workdate { get; set; }
+        [DataMember]
+        public int timeot_beforemin { get; set; }
+        [DataMember]
+        public int timeot_normalmin { get; set; }
+        [DataMember]
+        public int timeot_aftermin { get; set; }
+        [DataMember]
+        public string timeot_note { get; set; }
+        [DataMember]
+        public string location_code { get; set; }
+        [DataMember]
+        public string reason_code { get; set; }
+        [DataMember]
+        public string status { get; set; }
+        [DataMember]
+        public string modified_by { get; set; }
+        [DataMember]
+        public DateTime modified_date { get; set; }
+        [DataMember]
+        public string timeot_fromdate { get; set; }
+        [DataMember]
+        public string timeot_todate { get; set; }
+        [DataMember]
+        public bool flag { get; set; }
+        [DataMember]
+        public string ot_data { get; set; }
+    }
+    #endregion
 }
