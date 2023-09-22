c42cd3b4d3327e08666d878d49ced3ff604dc11f
Narakorn Sealee | นรากร  แซ่ลี้ <narakorn.s@bhatarapro.com>
2023-09-22 18:46:16 +0700

    foretype

--- BPC_OPR/ClassLibrary_BPC/hrfocus.model/employee/cls_MTForetype.cs
+++ BPC_OPR/ClassLibrary_BPC/hrfocus.model/employee/cls_MTForetype.cs
@@ -6,29 +6,18 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary_BPC.hrfocus.model
 {
-    public class cls_TRForeignercard
+    public class cls_MTForetype
     {
-        public cls_TRForeignercard() { }
-
+        public cls_MTForetype() { }
+        public int foretype_id { get; set; }
+        public string foretype_code { get; set; }
+        public string foretype_name_th { get; set; }
+        public string foretype_name_en { get; set; }
         public string company_code { get; set; }
-        public string worker_code { get; set; }
-
-        public int foreignercard_id { get; set; }
-        public string foreignercard_code { get; set; }
-
-        public string foreignercard_type { get; set; }
-
-        public string foreignercard_issue { get; set; }
-        public string foreignercard_expire { get; set; }
-
-
         public string created_by { get; set; }
         public DateTime created_date { get; set; }
-
-
         public string modified_by { get; set; }
         public DateTime modified_date { get; set; }
-
         public bool flag { get; set; }
     }
 }
